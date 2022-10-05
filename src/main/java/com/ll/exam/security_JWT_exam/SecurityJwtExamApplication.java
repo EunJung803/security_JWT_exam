b11cd52fa68e132c142ae07f147fdaccdffda563
EunJung803 <aimee3322@gmail.com>
2022-10-05 16:04:37 +0900

    Feat : POST /member/login 이 로그인 URL 테스트 환경에서 회원 2명 생성

--- src/main/java/com/ll/exam/security_JWT_exam/SecurityJwtExamApplication.java
+++ src/main/java/com/ll/exam/security_JWT_exam/SecurityJwtExamApplication.java
@@ -2,12 +2,20 @@ package com.ll.exam.security_JWT_exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SecurityJwtExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityJwtExamApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
