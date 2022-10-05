492ec44a749f6c4078f775a388ea12b8bc7b8112
EunJung803 <aimee3322@gmail.com>
2022-10-05 14:14:05 +0900

    setting

---
+++ src/main/java/com/ll/exam/security_JWT_exam/SecurityJwtExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.security_JWT_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtExamApplication.class, args);
+	}
+
+}
