d3f5ceae663fa36ab0fecc8ccfe88de6ad74fe73
EunJung803 <aimee3322@gmail.com>
2022-10-07 00:44:56 +0900

    Feat : 입력 데이터 유효성 체크 POST /member/login 으로 올바르지 않은 username과 password 데이터를 넘기면 400

--- src/main/java/com/ll/exam/security_JWT_exam/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/security_JWT_exam/app/member/controller/MemberController.java
@@ -16,6 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authentication", "JWT키");
 
@@ -28,5 +33,9 @@ public class MemberController {
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
+        }
     }
 }
