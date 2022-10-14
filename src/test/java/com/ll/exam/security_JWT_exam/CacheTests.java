c3123da8bdbea546e26a630befc548524f2cf732
EunJung803 <aimee3322@gmail.com>
2022-10-14 16:17:06 +0900

    Feat/Test : @CacheEvict 로 캐시 데이터 삭제

--- src/test/java/com/ll/exam/security_JWT_exam/CacheTests.java
+++ src/test/java/com/ll/exam/security_JWT_exam/CacheTests.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
@@ -22,13 +23,24 @@ class CacheTests {
     @DisplayName("캐시 사용")
     void t1() throws Exception {
         int rs = memberService.getCachedInt();
-
-        assertThat(rs).isGreaterThan(0);
         System.out.println(rs);
 
         rs = memberService.getCachedInt();
+        System.out.println(rs);
+    }
 
-        assertThat(rs).isGreaterThan(0);
+    @Test
+    @DisplayName("캐시 삭제")
+    void t2() throws Exception {
+        int rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        memberService.deleteCacheKey1();
+
+        rs = memberService.getCachedInt();
         System.out.println(rs);
     }
 }
